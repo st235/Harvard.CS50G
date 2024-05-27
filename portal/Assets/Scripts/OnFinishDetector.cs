b4764678ce55b64a78c6fa528efa01ba1f5619b8
Alex Dadukin <alexanderdadukin@gmail.com>
2024-05-27 20:00:44 +0100

    Portal implement platform level

---
+++ portal/Assets/Scripts/OnFinishDetector.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnFinishDetector : MonoBehaviour {
+
+    [SerializeField]
+    private Text _finalText;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Finish") {
+            Debug.Log("Hello world!");
+
+            _finalText.enabled = true;
+        }
+    }
+}
